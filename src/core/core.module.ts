a5359664d561ca4e312d85ddf3fd1a8d234d1d5e
Yash Patel <yashpatel07@gmail.com>
2020-05-24 20:59:29 -0700

    added header and fontawesome

---
+++ src/core/core.module.ts
@@ -0,0 +1,11 @@
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+    imports: [],
+    declarations: [NavBarComponent],
+    providers: [],
+    exports: [NavBarComponent]
+})
+
+export class CoreModule {}
