8c3db884b6f7b852a7bcc96478e25d6dc7b225f3
Yash Patel <yashpatel07@gmail.com>
2020-05-27 20:55:35 -0700

    module update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,13 @@
-import { CoreModule } from './../core/core.module';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { SharedModule } from "./../shared/shared.module";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    CoreModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, SharedModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/shared/shared.module.ts
@@ -0,0 +1,11 @@
+import { NavBarComponent } from "./components/nav-bar/nav-bar.component";
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [NavBarComponent],
+  providers: [],
+  exports: [NavBarComponent],
+})
+export class SharedModule {}
